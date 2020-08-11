9d9973f1650703e736df0ae107b118a09c2374f2
cchcham <69190682+cchcham@users.noreply.github.com>
2020-08-11 21:20:10 +0800

    Circular array Adt

--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -3,4 +3,26 @@ package entity;
 public class Player {
     String name ;
     String id;
+
+    public Player(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
 }
