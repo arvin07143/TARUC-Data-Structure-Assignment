25ebba8a641b4ab508ccfc90e86796e7bf547d20
jeffreyoh00 <jeffreyohzzz@gmail.com>
2020-08-11 21:28:39 +0800

    updated player class

--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -1,6 +1,33 @@
 package entity;
 
 public class Player {
-    String name ;
-    String id;
+    private String name ;
+    private String id;
+    private int hedgehogCount;
+    
+    //Constructors
+    public Player (){
+    }
+    public Player (String name, String id, int hedgehogCount){
+        this.name = name;
+        this.id = id;
+    }
+
+    //Get methods
+    public String getName(){
+        return name;
+    }
+    public String getId(){
+        return id;
+    }
+
+    //Set methods
+    public void SetName(String name){
+        this.name = name;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+   
+   
 }
