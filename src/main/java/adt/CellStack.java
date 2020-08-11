29b15e271660bdc50b6e6538eb14fde0531c871b
jeffreyoh00 <jeffreyohzzz@gmail.com>
2020-08-11 22:42:34 +0800

    Update CellStack.java

--- src/main/java/adt/CellStack.java
+++ src/main/java/adt/CellStack.java
@@ -5,9 +5,30 @@ package adt;
  * @author GV62 7RC
  */
 public class CellStack<T> implements StackInterface<T>{
-    public void push(T newEntry);
-    public T pop();
-    public T peek();
-    public boolean isEmpty();
-    public void clear();
+
+    @Override
+    public void push(T newEntry) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public T pop() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public T peek() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+   
 }
