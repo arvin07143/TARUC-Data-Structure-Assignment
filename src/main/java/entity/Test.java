9ea6fb1261f982a89e9be39d0f4ea0629c5a7427
kityao0811 <69194034+kityao0811@users.noreply.github.com>
2020-08-16 23:27:28 +0800

    ky

--- src/main/java/entity/Test.java
+++ src/main/java/entity/Test.java
@@ -8,7 +8,17 @@ public class Test {
     public static void main (String[] args){
         StackInterface<Hedgehog> playerMovement = new ArrayStack<>();
         Hedgehog store = new Hedgehog();
-        Cell[][] boardGrid = new Cell[20][20];
+        
+        Cell[][] boardGrid = new Cell[4][8];
+        for(int i = 0 ; i < 4 ; i++){
+            for (int y = 0 ; y <8 ; y++){
+                boardGrid[i][y] = new Cell(false,1);
+            }
+        }
+        boardGrid[1][1].setObstacleEnabled(true);
+        boardGrid[1][5].setObstacleEnabled(true);
+        boardGrid[2][1].setObstacleEnabled(true);
+        boardGrid[3][6].setObstacleEnabled(true);
         
         Player p1 = new Player("Loke","0001",4);
         p1.setHedgeHog(1, 1, 1);
@@ -27,19 +37,21 @@ public class Test {
         playerMovement.push(p2.getHedgehogs(2));
         p2.setHedgeHog(3, 3, 3);
         playerMovement.push(p2.getHedgehogs(3));
-        p2.setHedgeHog(4, 4, 4);
+        p2.setHedgeHog(4, 4, 5);
         playerMovement.push(p2.getHedgehogs(4));
-        store = store.chg(playerMovement.peek());
+        //store = store.chg(playerMovement.peek());
         
-        p2.setHedgeHog(4, 4, 4);
-        playerMovement.push(p2.getHedgehogs(4));
-        store.undo(p2, 4, store);
+        //p2.setHedgeHog(4, 4, 4);
+        //playerMovement.push(p2.getHedgehogs(4));
+        //store.undo(p2, 4, store);
         
         //System.out.println(playerStack.peek());
         //store.showPreviousMovement(playerMovement);
         //store.viewAllMovement(playerMovement);
-        store.moveX(p2, 4, playerMovement, boardGrid);
-        System.out.println(p2.getHedgehogs(4).getColumn());
-        System.out.println(p2.getHedgehogs(4).getRow());
+        //store.moveForward(p2, 4, playerMovement, boardGrid);
+        //System.out.println(p2.getHedgehogs(4).getColumn());
+        //System.out.println(p2.getHedgehogs(4).getRow());
+        store.moveVertical(p2, 1, playerMovement, boardGrid); // 2 2
+        System.out.println(p2.getHedgehogs(1).getRow() + " " + p2.getHedgehogs(1).getColumn());
     }
 }
