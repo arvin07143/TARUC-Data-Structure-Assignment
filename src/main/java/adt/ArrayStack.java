21bd0d8eeaf25999ee0df18dc9089682fe0756af
kityao0811 <69194034+kityao0811@users.noreply.github.com>
2020-08-23 13:47:56 +0800

    Update ArrayStack.java

--- src/main/java/adt/ArrayStack.java
+++ src/main/java/adt/ArrayStack.java
@@ -62,7 +62,7 @@ public class ArrayStack<T> implements StackInterface<T> {
     @Override
     public StackInterface<T> reverse() {
         StackInterface<T> reverseStack = new ArrayStack<>();
-        if (topIndex > 0) {
+        if (topIndex >= 0) {
             for (int i = topIndex + 1; i >= 0; i--) {
                 reverseStack.push(array[i]);
             }
